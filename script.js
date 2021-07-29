84f70a98de49dd83f979434bb9141d1b5dbd4bcc
unknown <fran.senes@gmail.com>
2021-07-29 17:59:29 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+function tiempo(){
+  
+    
+    
+        
+    var horas, minutos, segundos, a;
+    var  Tiempo = new Date();
+        horas = parsetime(Tiempo.getHours()) ;
+
+        minutos = parsetime(Tiempo.getMinutes()) ;
+
+        segundos = parsetime(Tiempo.getSeconds()) ;
+        a = horas +":"+minutos+":"+ segundos;
+
+        document.getElementById("time").innerHTML = a;
+  
+    
+        
+}
+function parsetime(i)
+{
+    if ( i < 10) 
+    {
+       i = "0" + i;
+    } 
+    return i;
+}
+setInterval(tiempo, 1000);
+
+
+
